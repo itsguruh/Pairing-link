443bd472001d744c50926dd6d859fa132acee4f8
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-07-12 13:03:38 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- pair.js
+++ pair.js
@@ -97,7 +97,7 @@ router.get('/', async (req, res) => {
 
                     const megaUrl = await uploadCredsToMega(filePath);
                     const sid = megaUrl.includes("https://mega.nz/file/")
-                        ? 'Silva~' + megaUrl.split("https://mega.nz/file/")[1]
+                        ? 'Caseyrhodes~' + megaUrl.split("https://mega.nz/file/")[1]
                         : 'Error: Invalid URL';
 
                     console.log(`Session ID: ${sid}`);
