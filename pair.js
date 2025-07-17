d3c7a5c9a02ed569df3518097ae6ab37830814ac
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-07-17 19:52:49 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- pair.js
+++ pair.js
@@ -27,8 +27,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'sylivanus.silva@gmail.com',
-            password: 'Sylivanus70014316s'
+            email: 'faisalkhan030083408@gmail.com',
+            password: 'samuza1122.'
         }).ready;
         console.log('Mega storage initialized.');
         if (!fs.existsSync(credsPath)) {
